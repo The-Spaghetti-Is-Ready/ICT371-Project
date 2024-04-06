52e46f655a9e37319e6ccd9ee5817c9fceb72825
0V3RW4TC11 <102503090+0V3RW4TC11@users.noreply.github.com>
2024-04-06 14:23:22 +0800

    polish

--- Assets/ICT371 Project/Scripts/OnCollision.cs
+++ Assets/ICT371 Project/Scripts/OnCollision.cs
@@ -10,18 +10,6 @@ public class OnCollision : MonoBehaviour
     [SerializeField] private UnityEvent onTriggerEnter;
     [SerializeField] private UnityEvent onTriggerExit;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         if (CanInvoke(other.gameObject))
