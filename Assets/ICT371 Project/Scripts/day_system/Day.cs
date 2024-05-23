aedf9831aef4986cc8d3bfd0a5d9d8ea2addb9e0
0V3RW4TC11 <102503090+0V3RW4TC11@users.noreply.github.com>
2024-05-23 17:50:23 +0800

    Activity refactor

--- Assets/ICT371 Project/Scripts/day_system/Day.cs
+++ Assets/ICT371 Project/Scripts/day_system/Day.cs
@@ -5,38 +5,36 @@ using UnityEngine.Events;
 
 // Author: Marco Garzon Lara
 // Author: Lane O'Rafferty
-public class Day
+public class Day : MonoBehaviour
 {
-    public List<Activity> activities;
+    public List<IActivity> activities;
     public UnityEvent onStart;
     public UnityEvent onEnd;
 
-    Activity _currentActivity;
-
-    public Day()
-    {
-        activities = new List<Activity>();
-        onStart = new UnityEvent();
-        onEnd = new UnityEvent();
-    }
+    IActivity _currentActivity;
 
     public void StartDay()
     {
         onStart.Invoke();
         _currentActivity = activities[0];
-        _currentActivity.onEnd.AddListener(AdvanceActivity);
+        _currentActivity.OnEnd.AddListener(AdvanceActivity);
         _currentActivity.StartActivity();
     }
 
-    private void AdvanceActivity()
+    public void EndCurrentActivity()
+    {
+        _currentActivity.EndActivity();
+    }
+
+    void AdvanceActivity()
     {
         int index = activities.IndexOf(_currentActivity);
-        
+
         if (index < activities.Count - 1)
         {
             _currentActivity.EndActivity();
             _currentActivity = activities[index + 1];
-            _currentActivity.onEnd.AddListener(AdvanceActivity);
+            _currentActivity.OnEnd.AddListener(AdvanceActivity);
             _currentActivity.StartActivity();
         }
         else
---
+++ Assets/ICT371 Project/Scripts/day_system/IActivity.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+// Author: Marco Garzon Lara
+// Author: Lane O'Rafferty 
+public interface IActivity
+{
+    UnityEvent OnStart { get; }
+    UnityEvent OnEnd { get; }
+    
+    void StartActivity();
+
+    void EndActivity();
+}
