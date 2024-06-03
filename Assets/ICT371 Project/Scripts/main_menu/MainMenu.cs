9f102f4543b314bb76da744c88a61d4a7ca020e7
0V3RW4TC11 <102503090+0V3RW4TC11@users.noreply.github.com>
2024-06-03 18:42:23 +0800

    added comments to MainMenu.cs

--- Assets/ICT371 Project/Scripts/main_menu/MainMenu.cs
+++ Assets/ICT371 Project/Scripts/main_menu/MainMenu.cs
@@ -4,16 +4,28 @@ using System.Collections.Generic;
 using UnityEditor.SceneManagement;
 using UnityEngine;
 
+/// <Author>
+/// Lane O'Rafferty
+/// </Author>
+/// <summary>
+/// This class is responsible for controlling the main menu.
+/// </summary>
 public class MainMenu : MonoBehaviour
 {
     [SerializeField]
     private int _gameSceneIndex = 0;
 
+    /// <summary>
+    /// Loads the game scene.
+    /// </summary>
     public void playGame()
     {
         EditorSceneManager.LoadScene(_gameSceneIndex);
     }
 
+    /// <summary>
+    /// Quits the game.
+    /// </summary>
     public void quitGame()
     {
     #if UNITY_EDITOR
