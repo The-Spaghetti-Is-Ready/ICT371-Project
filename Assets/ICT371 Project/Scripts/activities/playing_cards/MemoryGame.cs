780b86531345e7f4acaa2ce95b89cb21b1b3aab9
0V3RW4TC11 <102503090+0V3RW4TC11@users.noreply.github.com>
2024-06-03 17:41:49 +0800

    added comments to card game

--- Assets/ICT371 Project/Scripts/activities/playing_cards/MemoryGame.cs
+++ Assets/ICT371 Project/Scripts/activities/playing_cards/MemoryGame.cs
@@ -3,16 +3,33 @@ using UnityEngine.Events;
 using UnityEngine.XR.Interaction.Toolkit;
 using Random = System.Random;
 
+/// <author>
+/// Jaiden di Lanzo
+/// </author>
+/// <summary>
+/// This class models the card activity minigame and implements the IActivity interface.
+/// </summary>
+/// <seealso cref="IActivity"/>
+/// <seealso cref="PlayingCard"/>
 public class MemoryGame : MonoBehaviour, IActivity
 {
+    /// <summary>
+    /// Instance of the MemoryGame class.
+    /// </summary>
     public static MemoryGame Instance;
 
+    /// <summary>
+    /// Getter/Setter for the activity name
+    /// </summary>
     public string ActivityName
     {
         get => activityName;
         set => activityName = value;
     }
 
+    /// <summary>
+    /// Ends the activity.
+    /// </summary>
     public void EndActivity()
     {
         if (!_isRunning)
@@ -24,6 +41,9 @@ public class MemoryGame : MonoBehaviour, IActivity
         onEnd.Invoke();
     }
 
+    /// <summary>
+    /// Getter for the amount of moves made.
+    /// </summary>
     public int GetMoves()
     {
         return _moves;
@@ -47,6 +67,10 @@ public class MemoryGame : MonoBehaviour, IActivity
         set => onStart = value;
     }
 
+    /// <summary>
+    /// Selects a card.
+    /// </summary>
+    /// <param name="card">The card to select.</param>
     public void Select(PlayingCard card)
     {
         if (!_selectionTwo)
@@ -65,6 +89,9 @@ public class MemoryGame : MonoBehaviour, IActivity
         }
     }
 
+    /// <summary>
+    /// Starts the activity.
+    /// </summary>
     public void StartActivity()
     {
         _isRunning = true;
@@ -78,11 +105,18 @@ public class MemoryGame : MonoBehaviour, IActivity
         onStart.Invoke();
     }
 
+    /// <summary>
+    /// Gets a random element from the deck
+    /// </summary>
+    /// <typeparam name="T">The deck</typeparam>
     private static T GetRandomElement<T>(T[] array)
     {
         return array[(int)Mathf.Floor(UnityEngine.Random.value * array.Length)];
     }
 
+    /// <summary>
+    /// Checks if the two selected cards match.
+    /// </summary>
     private void CheckMatch()
     {
         if (_selectionOne.Matches(_selectionTwo))
@@ -100,6 +134,9 @@ public class MemoryGame : MonoBehaviour, IActivity
         _selectionOne = _selectionTwo = null;
     }
 
+    /// <summary>
+    /// Checks if the game has been won or lost.
+    /// </summary>
     private void CheckWon()
     {
         if (_matches == _deck.Length / 2)
@@ -117,6 +154,9 @@ public class MemoryGame : MonoBehaviour, IActivity
         }
     }
 
+    /// <summary>
+    /// Deals the cards.
+    /// </summary>
     private void Deal()
     {
         var n = 0;
@@ -130,6 +170,9 @@ public class MemoryGame : MonoBehaviour, IActivity
         }
     }
 
+    /// <summary>
+    /// Shuffles the deck.
+    /// </summary>
     private void Shuffle()
     {
         var rnd = new Random();
@@ -139,7 +182,7 @@ public class MemoryGame : MonoBehaviour, IActivity
             (_deck[i], _deck[n]) = (_deck[n], _deck[i]);
         }
     }
-
+    
     private void Start()
     {
         Instance = this;
@@ -160,30 +203,83 @@ public class MemoryGame : MonoBehaviour, IActivity
                 CheckMatch();
     }
 
+    /// <summary>
+    /// Deducts the time limit seconds.
+    /// </summary>
     private void DeductTimeLimitSeconds()
     {
         _timeLimitSeconds -= 1;
     }
 
+    /// <summary>
+    /// The suits of the deck.
+    /// </summary>
     private static readonly string[] Suits = { "Club", "Diamond", "Heart", "Spade" };
 
+    /// <summary>
+    /// The ranks of the deck.
+    /// </summary>
     private static readonly string[] Ranks =
         { "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13" };
 
+    /// <summary>
+    /// The name of the activity.
+    /// </summary>
     [SerializeField] private string activityName = "Memory Game";
+
+    /// <summary>
+    /// The won state of the game.
+    /// </summary>
     [SerializeField] private bool isWon;
+
+    /// <summary>
+    /// The events invoked when the activity ends.
+    /// </summary>
     [SerializeField] private UnityEvent onEnd;
+
+    /// <summary>
+    /// The events invoked when the activity starts.
+    /// </summary>
     [SerializeField] private UnityEvent onStart;
     
+    /// <summary>
+    /// The time limit in seconds.
+    /// </summary>
     [SerializeField] [Range(1, 30)]
     private int _timeLimitSeconds = 10;
 
+    /// <summary>
+    /// The deck of cards.
+    /// </summary>
     private PlayingCard[] _deck;
+    
+    /// <summary>
+    /// The amount of matches made.
+    /// </summary>
     private int _matches;
+
+    /// <summary>
+    /// The amount of moves made.
+    /// </summary>
     private int _moves;
+
+    /// <summary>
+    /// The first selected card.
+    /// </summary>
     private PlayingCard _selectionOne;
+
+    /// <summary>
+    /// The second selected card.
+    /// </summary>
     private PlayingCard _selectionTwo;
+
+    /// <summary>
+    /// The timer.
+    /// </summary>
     private double _timer;
 
+    /// <summary>
+    /// The running state of the game.
+    /// </summary>
     private bool _isRunning = false;
 }
